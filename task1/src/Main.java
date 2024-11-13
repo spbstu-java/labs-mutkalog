41a131001765ecb0ef5aad53b704b7641b6a0745
Alexander <pochtaforpsn@gmail.com>
2024-11-13 20:21:16 +0300

    Добавлена интерактивность.

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Hero regularHero = new Hero();
-        regularHero.move(new Crawl());
-        regularHero.move(new Run());
-        regularHero.move(new Cycling());
+        System.out.println("Press 'w' to crawl, 'r' to run, 'c' to cycle and 'q' to quit");
+
+        Scanner scanner = new Scanner(System.in);
+        char selectChar;
+
+        while (true) {
+            selectChar = scanner.next().charAt(0);
+            switch (selectChar){
+                case 'w':
+                    regularHero.move(new Crawl());
+                    break;
+                case 'r':
+                    regularHero.move(new Run());
+                    break;
+                case 'c':
+                    regularHero.move(new Cycling());
+                    break;
+                case 'q':
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Regular Hero does not have that type of movement");
+                    break;
+            }
+        }
     }
 }
 
